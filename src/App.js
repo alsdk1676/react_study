81af6213fd6db9b2bcb9ce53c61d4d398b41158f
alsdk1676 <alsdk6761@gmail.com>
2024-10-23 10:42:29 +0900

    day03

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import Check from './docs/ref/Check';
-import Chekc, { useRef } from 'react';
+// import Check from './docs/ref/Check';
+// import Chekc, { useRef } from 'react';
 // import Jsx01, { Jsx02, Jsx03 } from './docs/components/Jsx01';
 
 function App() { 
   return (
     <>
-    <Check/>
-
+      </>
     </>
   );
 }
